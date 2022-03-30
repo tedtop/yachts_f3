d51d7bfe2e9ba81339cae37122decc7e7ab43bee
Ted Toporkov <tedtoporkov@gmail.com>
2022-03-30 23:37:25 +0300

    Added templates for listings & details

--- index.php
+++ index.php
@@ -1,24 +1,96 @@
 <?php
 require 'vendor/autoload.php';
 $f3 = \Base::instance();
+$db = new DB\SQL(
+    'mysql:host=localhost;port=3306;dbname=yachts_ci',
+    'root',
+    'root'
+);
+// $db = new DB\SQL(
+//     'mysql:host=localhost;port=3306;dbname=charscom_yachts',
+//     'charscom_yachts',
+//     'charscom_yachts'
+// );
 
-$f3->route('GET /',
-    function($f3) {
-        $json = file_get_contents('assets/apollo_listings.json');
+$f3->route(
+    'GET /listings',
+    function ($f3) use ($db) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
+        $listings = json_decode($json, false);
+
+        $locations = $db->exec('SELECT * FROM location ORDER BY nice_name');
+        $f3->set('locations', $locations);
+
+        $f3->set('listings', $listings);
+        echo View::instance()->render('templates/panagea_list.phtml');
+    }
+);
+
+$f3->route(
+    'GET /listings/@id',
+    function ($f3) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
+        $listings = json_decode($json, false);
+
+        $f3->set('listing', $listings[$f3->get('PARAMS.id') - 1]);
+        echo View::instance()->render('templates/panagea_detail.phtml');
+    }
+);
+
+$f3->route(
+    'GET /locations',
+    function ($f3) use ($db) {
+        $f3->set('locations', $db->exec('SELECT * FROM location ORDER BY nice_name'));
+        echo Template::instance()->render('templates/locations.phtml');
+
+        // $json = file_get_contents('assets/data/apollo_listings.json');
+        // $yachts = json_decode($json, false);
+
+        // $f3->set('locations', $locations);
+        // echo View::instance()->render('templates/paginated.phtml');
+    }
+);
+
+$f3->route(
+    'GET /locations/@name',
+    function ($f3) use ($db) {
+
+        $sql = 'SELECT name FROM charterindex_listing
+                JOIN listing_location ON listing_location.listing_id = charterindex_listing.id
+                JOIN location on location.id = listing_location.location_id
+                WHERE location.key = "' . $f3->get('PARAMS.name') . '"';
+
+        $listings = $db->exec($sql);
+        // echo count($listings) . " listings";
+        $f3->set('count', count($listings));
+        $f3->set('listings', $db->exec($sql));
+        echo Template::instance()->render('templates/locations_listings.phtml');
+
+        // $json = file_get_contents('assets/data/apollo_listings.json');
+        // $yachts = json_decode($json, false);
+
+        // $f3->set('yachts', $yachts);
+        // echo View::instance()->render('templates/paginated.phtml');
+    }
+);
+
+$f3->route(
+    'GET /locations/{name}/listings',
+    function ($f3) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
         $yachts = json_decode($json, false);
 
         $f3->set('yachts', $yachts);
         echo View::instance()->render('templates/paginated.phtml');
-        
-        // echo "<pre>";
-        // print_r($yachts);
-        // echo "</pre>";
     }
 );
 
-$f3->route('GET /location/@location',
-    function($f3) {
-        $json = file_get_contents('assets/apollo_listings.json');
+/* === APOLLO === */
+
+$f3->route(
+    'GET /location/@location',
+    function ($f3) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
         $yachts = json_decode($json, false);
 
         $filteredYachts = array();
@@ -33,13 +105,25 @@ $f3->route('GET /location/@location',
     }
 );
 
-$f3->route('GET /yacht/@id',
-    function($f3) {
-        $json = file_get_contents('assets/apollo_listings.json');
+$f3->route(
+    'GET /apollo/listings',
+    function ($f3) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
+        $yachts = json_decode($json, false);
+
+        $f3->set('yachts', $yachts);
+        echo View::instance()->render('templates/apollo_list.phtml');
+    }
+);
+
+$f3->route(
+    'GET /apollo/listings/@id',
+    function ($f3) {
+        $json = file_get_contents('assets/data/apollo_listings.json');
         $yachts = json_decode($json, false);
 
         $f3->set('yacht', $yachts[$f3->get('PARAMS.id') - 1]);
-        echo View::instance()->render('templates/single.phtml');
+        echo View::instance()->render('templates/apollo_single.phtml');
     }
 );
 
--- spiders/charterindex_spider.rb
+++ spiders/charterindex_spider.rb
@@ -5,13 +5,165 @@ require 'mysql2'
 require 'json'
 
 
-def length_converter(length_str)
-  ft_str = length_str.split(" / ", -1)[1]
-  return ft_str.split("ft", -1)[0]
+class CharterIndexSpider < Kimurai::Base
+  @name = "charterindex_spider"
+  @engine = :selenium_chrome
+  @start_urls = ["https://www.charterindex.com/search/yachts?categories=motor"] # OR categories=sailing CHANGE THIS
+  @config = {
+    user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+    retry_request_errors: [{ error: TimeoutError }],
+    disable_images: true,
+  }
+
+  def parse(response, url:, data: {})
+    yacht_link_path = "//a[@data-testid=\"yacht-card-link\"]"
+
+    all_links = Set[]
+    
+    yachts_urls = response.xpath(yacht_link_path).map {|data| data[:href]}
+    all_links.merge(yachts_urls)
+    browser.send_keys(:tab)
+    
+    counter = 0
+
+    loop do
+      browser.send_keys(:page_down) ; sleep (0.5 + (counter * 0.5))
+      
+      response = browser.current_response
+      
+      logger.info "> Current item count is #{all_links.size()}"
+
+      if all_links.size() > 1 
+        break
+      end
+
+      yachts_urls = response.xpath(yacht_link_path).map {|data| data[:href]}
+
+      if yachts_urls.to_set.subset?(all_links)
+        counter = counter + 1;
+        if counter == 5
+          break
+        end
+      else
+        counter = 0
+      end
+
+      all_links.merge(yachts_urls)
+    end
+
+    logger.info "> All links to yachts from page: #{yachts_urls.join('; ')}"
+
+    # yachts_urls.slice(0,2).each do |url|
+    #   request_to :parse_yacht_page, url: absolute_url(url, base: "https://www.charterindex.com/")
+    # end
+
+    # in_parallel(:parse_yacht_page, yachts_urls.map{|url| absolute_url(url, base: "https://www.charterindex.com/")}, threads: 3)
+  end
+
+  def parse_yacht_page(response, url:, data: {})
+    item = {}
+
+    item[:url] = url
+    item[:name] = response.xpath("//*[@id=\"layout\"]/main/div[1]/div[2]/div/div[1]/h1").text.squish
+    item[:title] = response.title
+    item[:meta_description] = response.at_css("meta[name='description']").attr('content')
+    # item[:meta_keywords]
+
+    item[:header_image] = response.xpath('//*[@id="layout"]/main/div[1]/div[1]/div/span/img')[0][:src]
+
+    item[:key_details] = {
+      length: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[2]/p').text.squish,
+      builder: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[11]/p').text.squish,
+      sleeps: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[2]/div[1]/span[1]/span').inner_html,
+      cabins: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[2]/div[2]/span[1]/span').inner_html,
+      built: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[10]/p').text.squish,
+      #refit: 
+      price: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[3]/span[1]').inner_html
+    }
+
+    item[:operating_in] = "Operating in " + response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[1]/section[2]/div/a')
+    .map.with_index {|data, index| 
+        response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[2]/div/a[#{index + 1}]/div/span[1]")[0].inner_html
+    }
+    .join(", ")
+
+    item[:locations] = response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[1]/section[2]/div/a')
+    .map.with_index {|data, index| 
+        response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[2]/div/a[#{index + 1}]/div/span[1]")[0].inner_html
+    }
+
+    item[:about_html] = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[1]/div/div/div")&.inner_html&.squish.to_s
+    item[:about_text] = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[1]/div/div/div")&.text&.squish.to_s
+
+    # This does not always produce a video url
+    possible_video_element = response.at_css("div.viewport > div.slides > a:nth-child(1)")
+
+    if possible_video_element.attr('srl_video_thumbnail') 
+      item[:video_url] = possible_video_element.attr('href').to_s
+    else
+      item[:video_url] = "" 
+    end
+
+    image_count = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a").count
+
+    item[:image_urls] = (1..image_count).map{|index|
+      {
+        # Keep in mind that not all images have an alt specified
+        caption: response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a[#{index}]/img")[0][:alt],
+        url: response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a[#{index}]/img")[0][:src]
+      }
+    }
+
+    item[:specs] = response.css('section.wide.no-border > div > ul > li')
+    .map {
+      |row|
+      {
+        heading: row.at_css('span').inner_html.to_s,
+        detail: row.at_css('p').text.squish
+      }
+    }
+
+    item[:amenities] = {
+      general: response.css('#tabpanel-amenities > ul > li')&.map {|li| li.text},
+      electrical: response.css('#tabpanel-electronics > ul > li')&.map {|li| li.text},
+      toys: response.css('#tabpanel-toys > ul > li')&.map {|li| li.text},
+      tenders: response.css('#tabpanel-diving > ul > li')&.map {|li| li.text},
+      diving: response.css('#tabpanel-tenders > ul > li')&.map {|li| li.text}
+    }
+
+    # I have not seen any crew image
+    item[:crew_image] = ""
+    crew_profiles = response.css("section:nth-child(5) > div > div.MuiPaper-elevation").map do | profile |
+      attributes = {
+        title: profile.at_css("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span").text.squish + ": " + profile.at_css("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p").text.squish,
+        full_html: profile.to_html.squish,
+        bio_text: profile.at_css("p.MuiTypography-root.MuiTypography-body2").text.squish
+      }
+      if bio_photo = profile.at_css('span > img')&.attr('src')
+        attributes[:bio_photo] = bio_photo # this does not work. I do not know how to get it.
+      end
+      attributes
+    end
+    item[:crew_profiles] = crew_profiles unless crew_profiles.empty?
+    
+    # I do not know what layout is supposed to be
+    #item[:layout] = 
+
+    # save_to "result.json", item, format: :pretty_json 
+    client = DBClient.new()
+    client.insert_listing(item, response.to_html)
+  end
+
+  if listing_url = ARGV[0]
+    CharterIndexSpider.parse!(:parse_yacht_page, url: listing_url)
+  else
+    CharterIndexSpider.crawl!
+  end
 end
 
+# CharterIndexSpider.crawl!
+
 class DBClient
-  
   def initialize(host: "charterboats.com", username: "charscom_yachts", password: "charscom_yachts")
     @client = Mysql2::Client.new(:host => host, :username => username, :password => password)
   end
@@ -22,14 +174,12 @@ class DBClient
   end
 
   def insert_listing(listing, original_data)
-
     specs = listing[:specs].reduce({}) {|map, el| map.merge!({ el[:heading] => el[:detail] })}
     
     price = listing[:key_details][:price] != nil ? listing[:key_details][:price] : "" 
     price = price.tr('^0-9', '')
 
     puts listing[:amenities]
-
 
     params = {
       "source" => "charterindex",
@@ -90,12 +240,8 @@ class DBClient
 
     last_id = @client.query("SELECT LAST_INSERT_ID();").first.values[0]
   
-
     add_locations_to_id(last_id, listing[:locations])
-
-
   end
-
 
   def get_id_of_location(location)
     location.sub! "&amp;", "&"
@@ -109,9 +255,7 @@ class DBClient
     return a
   end
 
-
   def add_locations_to_id(id, locations)
-
     statement_str = p %{
       INSERT INTO charscom_yachts.listing_location (listing_id, location_id) values (?, ?);
     }.gsub(/\s+/, " ").strip
@@ -125,167 +269,9 @@ class DBClient
       end
     }
   end
-  
+
+  def length_converter(length_str)
+    ft_str = length_str.split(" / ", -1)[1]
+    return ft_str.split("ft", -1)[0]
+  end
 end
-
-
-class CharterIndexSpider < Kimurai::Base
-  @name = "charterindex_spider"
-  @engine = :selenium_chrome
-  @start_urls = ["https://www.charterindex.com/search/yachts?categories=motor"] # OR categories=sailing CHANGE THIS
-  @config = {
-    user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
-    retry_request_errors: [{ error: TimeoutError }],
-    disable_images: true,
-  }
-
-  def parse(response, url:, data: {})
-    yacht_link_path = "//a[@data-testid=\"yacht-card-link\"]"
-
-    all_links = Set[]
-    
-    yachts_urls = response.xpath(yacht_link_path).map {|data| data[:href]}
-    all_links.merge(yachts_urls)
-    browser.send_keys(:tab)
-    
-    counter = 0
-
-    loop do
-      browser.send_keys(:page_down) ; sleep (0.5 + (counter * 0.5))
-      
-      
-      response = browser.current_response
-      
-      logger.info "> Current item count is #{all_links.size()}"
-
-      if all_links.size() > 1 
-        break
-      end
-
-      yachts_urls = response.xpath(yacht_link_path).map {|data| data[:href]}
-
-      if yachts_urls.to_set.subset?(all_links)
-        counter = counter + 1;
-        if counter == 5
-          break
-        end
-      else
-        counter = 0
-      end
-
-      all_links.merge(yachts_urls)
-    end
-
-    logger.info "> All links to yachts from page: #{yachts_urls.join('; ')}"
-
-    # yachts_urls.slice(0,2).each do |url|
-    #   request_to :parse_yacht_page, url: absolute_url(url, base: "https://www.charterindex.com/")
-    # end
-
-    in_parallel(:parse_yacht_page, yachts_urls.map{|url| absolute_url(url, base: "https://www.charterindex.com/")}, threads: 3)
-
-  end
-
-  def parse_yacht_page(response, url:, data: {})
-    item = {}
-
-    item[:url] = url
-    item[:name] = response.xpath("//*[@id=\"layout\"]/main/div[1]/div[2]/div/div[1]/h1").text.squish
-    item[:title] = response.title
-    item[:meta_description] = response.at_css("meta[name='description']").attr('content')
-    # item[:meta_keywords]
-
-    item[:header_image] = response.xpath('//*[@id="layout"]/main/div[1]/div[1]/div/span/img')[0][:src]
-
-    item[:key_details] = {
-      length: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[2]/p').text.squish,
-      builder: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[11]/p').text.squish,
-      sleeps: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[2]/div[1]/span[1]/span').inner_html,
-      cabins: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[2]/div[2]/span[1]/span').inner_html,
-      built: response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[2]/ul/li[10]/p').text.squish,
-      #refit: 
-      price: response.xpath('//*[@id="layout"]/main/div[1]/div[2]/div/div[3]/span[1]').inner_html
-    }
-
-    item[:operating_in] = "Operating in " + response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[1]/section[2]/div/a')
-    .map.with_index {|data, index| 
-        response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[2]/div/a[#{index + 1}]/div/span[1]")[0].inner_html
-    }
-    .join(", ")
-
-    item[:locations] = response.xpath('//*[@id="layout"]/main/div[2]/section[2]/div[1]/section[2]/div/a')
-    .map.with_index {|data, index| 
-        response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[2]/div/a[#{index + 1}]/div/span[1]")[0].inner_html
-    }
-
-    item[:about_html] = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[1]/div/div/div")&.inner_html&.squish.to_s
-    item[:about_text] = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[2]/div[1]/section[1]/div/div/div")&.text&.squish.to_s
-
-    # this does not always produce a video url
-    possible_video_element = response.at_css("div.viewport > div.slides > a:nth-child(1)")
-
-    if possible_video_element.attr('srl_video_thumbnail') 
-      item[:video_url] = possible_video_element.attr('href').to_s
-    else
-      item[:video_url] = "" 
-    end
-
-    image_count = response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a").count
-
-    item[:image_urls] = (1..image_count).map{|index|
-      {
-        # keep in mind that not all images have an alt specified
-        caption: response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a[#{index}]/img")[0][:alt],
-        url: response.xpath("//*[@id=\"layout\"]/main/div[2]/section[1]/div/div/div[1]/div/a[#{index}]/img")[0][:src]
-      }
-    }
-
-    item[:specs] = response.css('section.wide.no-border > div > ul > li')
-    .map {
-      |row|
-      {
-        heading: row.at_css('span').inner_html.to_s,
-        detail: row.at_css('p').text.squish
-      }
-    }
-
-    item[:amenities] = {
-      general: response.css('#tabpanel-amenities > ul > li')&.map {|li| li.text},
-      electrical: response.css('#tabpanel-electronics > ul > li')&.map {|li| li.text},
-      toys: response.css('#tabpanel-toys > ul > li')&.map {|li| li.text},
-      tenders: response.css('#tabpanel-diving > ul > li')&.map {|li| li.text},
-      diving: response.css('#tabpanel-tenders > ul > li')&.map {|li| li.text}
-    }
-
-    # i have not seen any crew image
-    item[:crew_image] = ""
-    crew_profiles = response.css("section:nth-child(5) > div > div.MuiPaper-elevation").map do | profile |
-      attributes = {
-        title: profile.at_css("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span").text.squish + ": " + profile.at_css("div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p").text.squish,
-        full_html: profile.to_html.squish,
-        bio_text: profile.at_css("p.MuiTypography-root.MuiTypography-body2").text.squish
-      }
-      if bio_photo = profile.at_css('span > img')&.attr('src')
-        attributes[:bio_photo] = bio_photo # this does not work. I do not know how to get it.
-      end
-      attributes
-    end
-    item[:crew_profiles] = crew_profiles unless crew_profiles.empty?
-    
-    # I do not know what layout is supposed to be
-    #item[:layout] = 
-
-    # save_to "result.json", item, format: :pretty_json 
-    client = DBClient.new()
-    client.insert_listing(item, response.to_html)
-  end
-
-  if listing_url = ARGV[0]
-    CharterIndexSpider.parse!(:parse_yacht_page, url: listing_url)
-  else
-    CharterIndexSpider.crawl!
-  end
-
-end
-
-# CharterIndexSpider.crawl!
